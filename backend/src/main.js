ff8cfaf07c93d065abed83ff370d59680846a257
Rodrimansidub14 <man22611@uvg.edu.gt>
2024-05-23 11:27:13 -0600

    Update main.js

--- backend/src/main.js
+++ backend/src/main.js
@@ -169,8 +169,7 @@ app.use((err, req, res, next) => {
 })
 
 const PORT = process.env.PORT || 5000
-const HOST = '0.0.0.0'
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
 })
