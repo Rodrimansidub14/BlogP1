dcbf926cbdcf3480171b0a46cf7a55d02d81ee2d
Rodrimansidub14 <man22611@uvg.edu.gt>
2024-05-23 13:15:47 -0600

    Update main.js

--- backend/src/main.js
+++ backend/src/main.js
@@ -170,5 +170,5 @@ app.use((err, req, res, next) => {
 
 const port = 5000
 app.listen(port, () => {
-  console.log(`Server listening at http://127.0.0.1:${port}`)
+  console.log(`Server listening at ${port}`)
 })
